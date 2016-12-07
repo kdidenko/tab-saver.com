1575478650927ecdc5ee5176fa2de96c99cbb062
Kostyantyn Didenko <kdidenko@gmail.com>
2016-12-07 23:32:08 +0000

    empty Express project created

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
-/**
- * @project     tab-saver.com
- * @author      Kostyantyn Didenko <kdidenko@ito-global.com>
- * @date        06.12.2016 11:17
- * @copyright   Copyright (C) 2008 - 2016 ITO-Global, LLC. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var express = require('express')
-var app = express()
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+var app = express();
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
