fc04acf322d4bbdf4f86bc39f4f56858c5eaa13b
Kostyantyn Didenko <kdidenko@gmail.com>
2017-03-02 21:01:21 +0200

    1. config package added; some code was modified accordingly to take variables from config file instead of hardcoding\n 2. some files formatting changed from spaces identation to tabs

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var config = require('config');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'Tab Saver - Chrome Extension',
-        host: req.header('host')
+        title: 'Tab Saver v2.0 - Launch is soon!',
+        host: config.get('host')
     });
 });
 
